31711e84e566903e15de7e31ae66f5c45b7dbdb8
Jordan JEAN <jordan73@live.fr>
2017-12-01 14:38:09 +0100

    saisie_utilisateur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DetailsPage } from '../pages/details/details';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DetailsPage
   ],
   imports: [
     BrowserModule,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+interface searchResult {
+  title: string;
+  author: string;
+  date: number;
+  image: string;
+}
+
+const RESULTS: Array<searchResult> = [{
+  title: "resultat1", author: "author1", date: 2017, image: '<img src="http://lorempixel.com/400/200" />'
+}, {
+  title: "resultat2", author: "author2", date: 2018, image: '<img src="http://lorempixel.com/400/200" />'
+}];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
+  listRes: Array<searchResult> = [];
+  query: string = '';
+  showNoRes: boolean = true;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onImput():void{
+    console.log(this.query);
+    if(this.query == ''){
+      this.listRes = [];
+      this.showNoRes = true;
+    }else{
+      this.showNoRes = false;
+      this.listRes = RESULTS;
+    }
   }
 
 }
